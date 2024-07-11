7b5cb27fd38b9188499f29416091bea9a4f92611
Admin <purohitparthiv@gmail.com>
2024-07-11 23:40:46 +0530

    try

--- src/components/App.js
+++ src/components/App.js
@@ -2,10 +2,18 @@
 import React from "react";
 import './../styles/App.css';
 
+var count = 0;
+let btn = document.querySelector('#btn1');
+btn.addEventListener("click", function(){
+   count = count +1;
+})
 const App = () => {
   return (
     <div>
         {/* Do not remove the main div */}
+        <p>Button clicked {count} times</p><br/>
+        <button id="btn1">Click me</button> 
+
     </div>
   )
 }
